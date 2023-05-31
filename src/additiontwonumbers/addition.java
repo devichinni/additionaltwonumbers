49880ff8a8015e9e272159f61ae6bd2ebbb0dfa0
Rasi Patnala <patnaladevi56@gmail.com>
2023-05-31 15:07:57 +0530

    javaadditional

---
+++ src/additiontwonumbers/addition.java
@@ -0,0 +1,13 @@
+package additiontwonumbers;
+
+public class addition {
+
+	public static void main(String[] args) {
+     int num1 = 20;
+     int num2 = 30;
+     int result;
+     result = num1+num2;
+     System.out.println("addition of two numbers"+ " "+result);
+	}
+
+}
